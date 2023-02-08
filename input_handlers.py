322b0d50c013c290a957a9f3282c2976fe5c8dfb
Perry Franks <perry.franks@unsw.edu.au>
2023-02-08 18:04:11 +1100

    up to change entity

--- input_handlers.py
+++ input_handlers.py
@@ -1,10 +1,27 @@
-from typing import Optional
-
+from __future__ import annotations
+from typing import Optional, TYPE_CHECKING
 import tcod.event
 from actions import Action, EscapeAction, BumpAction
 
+if TYPE_CHECKING:
+    from engine import Engine
 
 class EventHandler(tcod.event.EventDispatch[Action]):
+    def __init__(self, engine: Engine):
+        self.engine = engine
+
+    def handle_events(self) -> None:
+        for event in tcod.event.wait():
+            action = self.dispatch(event)
+
+            if action is None:
+                continue
+
+            action.perform()
+
+            self.engine.handle_enemy_turns()
+            self.engine.update_fov()
+
     def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
         raise SystemExit()
 
@@ -12,18 +29,19 @@ class EventHandler(tcod.event.EventDispatch[Action]):
         action: Optional[Action] = None
 
         key = event.sym
+        player = self.engine.player
 
         if key == tcod.event.K_UP or key == tcod.event.K_k:
-            action = BumpAction(dx=0, dy=-1)
+            action = BumpAction(player,dx=0, dy=-1)
         elif key == tcod.event.K_DOWN or key == tcod.event.K_j:
-            action = BumpAction(dx=0, dy=1)
+            action = BumpAction(player, dx=0, dy=1)
         elif key == tcod.event.K_LEFT or key == tcod.event.K_h:
-            action = BumpAction(dx=-1, dy=0)
+            action = BumpAction(player, dx=-1, dy=0)
         elif key == tcod.event.K_RIGHT or key == tcod.event.K_l:
-            action = BumpAction(dx=1, dy=0)
+            action = BumpAction(player, dx=1, dy=0)
 
         elif key == tcod.event.K_ESCAPE:
-            action = EscapeAction()
+            action = EscapeAction(player)
 
         # no valid key
         return action
