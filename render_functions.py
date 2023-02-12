93dd796baec6e9844b82f4b8d70d738b68b2169f
Perry Franks <perry.franks@unsw.edu.au>
2023-02-12 22:24:52 +1100

    up to part 8

--- render_functions.py
+++ render_functions.py
@@ -6,9 +6,24 @@ import color
 
 if TYPE_CHECKING:
     from tcod import Console
+    from engine import Engine
+    from game_map import GameMap
+
+
+def get_names_at_location(x: int, y: int, game_map: GameMap) -> str:
+    if not game_map.in_bounds(x, y) or not game_map.visible[x, y]:
+        return ""
+
+    names = ", ".join(
+        entity.name for entity in game_map.entities if entity.x == x and entity.y == y
+    )
+
+    print("get_names_at_location " + names)
+    return names.capitalize()
+
 
 def render_bar(
-        console: Console, current_value: int, maximum_value:int, total_width: int
+        console: Console, current_value: int, maximum_value: int, total_width: int
 ) -> None:
     bar_width = int(float(current_value) / maximum_value * total_width)
 
@@ -22,3 +37,15 @@ def render_bar(
     console.print(
         x=1, y=45, string=f"HP: {current_value}/{maximum_value}", bg=color.bar_filled
     )
+
+
+def render_names_at_mouse_location(
+        console: Console, x: int, y: int, engine: Engine
+) -> None:
+    mouse_x, mouse_y = engine.mouse_location
+
+    names_at_mouse_location = get_names_at_location(
+        x=mouse_x, y=mouse_y, game_map=engine.game_map
+    )
+
+    console.print(x=x, y=y, string=names_at_mouse_location)
