09bfd2dadeeb078b1da9eef3f8ca81644a0789e1
Perry Franks <perry.franks@unsw.edu.au>
2023-02-17 22:26:29 +1100

    up to part 10 saving

--- entity_factories.py
+++ entity_factories.py
@@ -37,10 +37,21 @@ health_potion = Item(
     name="Health Potion",
     consumable=consumable.HealingConsumable(amount=4),
 )
-
-lightning_scoll = Item(
+confusion_scroll = Item(
+    char="~",
+    color=(207, 63, 255),
+    name="Confusion Scroll",
+    consumable=consumable.ConfusionConsumable(number_of_turns=10),
+)
+lightning_scroll = Item(
     char="~",
     color=(255, 255, 0),
     name="Lightning Scroll",
     consumable=consumable.LightningDamageConsumable(damage=20, maximum_range=5),
+)
+fireball_scroll = Item(
+    char="~",
+    color=(255, 0, 0),
+    name="Fireball Scroll",
+    consumable=consumable.FireballDamageConsumable(damage=12, radius=3),
 )
--- procgen.py
+++ procgen.py
@@ -67,8 +67,12 @@ def place_entities(
             item_chance = random.random()
             if item_chance < 0.7:
                 entity_factories.health_potion.spawn(dungeon, x, y)
+            elif item_chance < 0.8:
+                entity_factories.fireball_scroll.spawn(dungeon, x, y)
+            elif item_chance < 0.9:
+                entity_factories.confusion_scroll.spawn(dungeon, x, y)
             else:
-                entity_factories.lightning_scoll.spawn(dungeon, x, y)
+                entity_factories.lightning_scroll.spawn(dungeon, x, y)
 
 
 def tunnel_between(
