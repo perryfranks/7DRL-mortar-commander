9c8a89eb900f0ac70182552b67c92990dcb4bbc8
Perry Franks <perry.franks@unsw.edu.au>
2023-02-19 16:01:40 +1100

    LevelUpEventHandler

--- procgen.py
+++ procgen.py
@@ -110,6 +110,8 @@ def generate_dungeon(
 
     rooms: List[RectangularRoom] = []
 
+    center_of_last_room = (0, 0)
+
     # stops after max_rooms attempts not after adding that many to the dungeon
     for r in range(max_rooms):
         room_width = random.randint(room_min_size, room_max_size)
@@ -138,7 +140,13 @@ def generate_dungeon(
             for x, y in tunnel_between(rooms[-1].center, new_room.center):
                 dungeon.tiles[x, y] = tile_types.floor
 
+            center_of_last_room = new_room.center
+
         place_entities(new_room, dungeon, max_monsters_per_room, max_items_per_room)
+
+        dungeon.tiles[center_of_last_room] = tile_types.down_stairs
+        dungeon.downstairs_location = center_of_last_room
+
         # append the new room to the list
         rooms.append(new_room)
 
--- render_functions.py
+++ render_functions.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import Tuple, TYPE_CHECKING
 
 import color
 
@@ -21,6 +21,15 @@ def get_names_at_location(x: int, y: int, game_map: GameMap) -> str:
     print("get_names_at_location " + names)
     return names.capitalize()
 
+def render_dungeon_level(
+        console: Console, dungeon_level: int, location: Tuple[int, int]
+) -> None:
+    """
+    Render the level the player is currently on, at the given location.
+    """
+    x, y = location
+
+    console.print(x=x, y=y, string=f"Dungeon level: {dungeon_level}")
 
 def render_bar(
         console: Console, current_value: int, maximum_value: int, total_width: int
