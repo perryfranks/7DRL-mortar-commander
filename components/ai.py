6cece77d0e6ba9b31cec55002771932662568985
Perry Franks <perry.franks@unsw.edu.au>
2023-03-07 00:17:25 +1100

    EnemySupplyScavenger being sketched out

--- components/ai.py
+++ components/ai.py
@@ -9,7 +9,7 @@ import tcod
 from actions import Action, BumpAction, MeleeAction, MovementAction, WaitAction
 
 if TYPE_CHECKING:
-    from entity import Actor
+    from entity import Actor, Entity
 
 
 class BaseAI(Action):
@@ -72,6 +72,42 @@ class HostileEnemy(BaseAI):
                 return None
             if distance <= 1:
                 return MeleeAction(self.entity, dx, dy).perform()
+            self.path = self.get_path_to(target.x, target.y)
+
+        if self.path:
+            dest_x, dest_y = self.path.pop(0)
+            return MovementAction(
+                self.entity, dest_x - self.entity.x, dest_y - self.entity.y
+            ).perform()
+
+        return WaitAction(self.entity).perform()
+
+
+class EnemySupplyScavenger(HostileEnemy):
+    """
+        Basically the HostileEnemy but will go for the nearest supplies instead of the player
+    """
+    def get_target(self) -> Optional[Entity]:
+        """
+        Get the nearest supplies to target
+        :return: Entity to target. From there you can extract x,y
+        """
+        pass
+
+    def get_supplies(self, entity: Entity) -> None:
+        pass
+
+    def perform(self) -> None:
+        # TODO: change this to nearest supplies
+        target = self.get_target()
+        dx = target.x - self.entity.x
+        dy = target.y - self.entity.y
+        distance = max(abs(dx), abs(dy))  # Chebyshev distance
+
+        if self.engine.game_map.visible[self.entity.x, self.entity.y]:
+            if distance == 0:
+                # grab the supplies I suppose
+                return None
             self.path = self.get_path_to(target.x, target.y)
 
         if self.path:
